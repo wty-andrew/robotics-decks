db7bf139161bb68ac84c0ce52d00172826bcb2fa
Andrew <wty.andrew@gmail.com>
2020-11-05 22:57:16 +0800

    :wrench: gatsby-theme-mdx-deck & eslint setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Gatsby MDX-Deck Template</h1>
+      <ul>
+        <li>
+          <Link to="/foo">Foo</Link>
+        </li>
+        <li>
+          <Link to="/bar">Bar</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
